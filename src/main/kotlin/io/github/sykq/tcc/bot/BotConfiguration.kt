5d71f2dea243e6cda20320a434f22aa48f9eca94
sykq <genericmailacc@gmail.com>
2021-07-25 22:36:34 +0200

    implement prototype for "Reactive"Bot

--- src/main/kotlin/io/github/sykq/tcc/bot/BotConfiguration.kt
+++ src/main/kotlin/io/github/sykq/tcc/bot/BotConfiguration.kt
@@ -5,6 +5,8 @@ import io.github.sykq.tcc.TmiMessage
 import io.github.sykq.tcc.TmiSession
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicInteger
@@ -12,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger
 @Configuration
 class BotConfiguration {
 
-    @Bean
+//    @Bean
     fun messageCountingBot(): Bot {
         return object : Bot {
             lateinit var startTime: LocalDateTime
@@ -25,8 +27,8 @@ class BotConfiguration {
                 session.tagCapabilities()
                 // manually join a channel; alternatively the channels member can be filled with all the channels which
                 // should be joined when connecting
-                session.join("sykq")
-                session.textMessage("sykq", "connected")
+                session.join("shenryyr")
+//                session.textMessage("sykq", "connected")
                 startTime = LocalDateTime.now()
             }
 
@@ -37,6 +39,44 @@ class BotConfiguration {
                         subscriberMessages.getAndIncrement()
                     }
                 }
+            }
+
+            override fun getProperties(): Map<String, Any> {
+                val messagesPerMinute =
+                    totalMessages.get() / Duration.between(startTime, LocalDateTime.now()).toMinutesExact()
+                return mapOf(
+                    "totalMessages" to totalMessages.get(),
+                    "subscriberMessages" to subscriberMessages.get(),
+                    "messagesPerMinute" to messagesPerMinute
+                )
+            }
+        }
+    }
+
+    @Bean
+    fun reactiveMessageCountingBot(): ReactiveBot {
+        return object : ReactiveBot {
+            lateinit var startTime: LocalDateTime
+            val totalMessages: AtomicInteger = AtomicInteger(0)
+            val subscriberMessages: AtomicInteger = AtomicInteger(0)
+
+            override val name: String = "reactiveMessageCountingBot"
+
+            override fun onConnect(session: ConfigurableTmiSession) {
+                session.tagCapabilities()
+                // manually join a channel; alternatively the channels member can be filled with all the channels which
+                // should be joined when connecting
+                session.join("shenryyr")
+//                session.textMessage("sykq", "connected")
+                startTime = LocalDateTime.now()
+            }
+
+            override fun onMessage(session: TmiSession, messages: Flux<TmiMessage>): Mono<Void> {
+                return messages.log()
+                    .doOnEach { totalMessages.getAndIncrement() }
+                    .filter { it.tags["subscriber"]?.contains("1") ?: false }
+                    .doOnEach { subscriberMessages.getAndIncrement() }
+                    .then()
             }
 
             override fun getProperties(): Map<String, Any> {
