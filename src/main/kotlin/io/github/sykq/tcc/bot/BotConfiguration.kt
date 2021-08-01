412dad8af1320b73d6895f59a069623d971775e6
sykq <genericmailacc@gmail.com>
2021-08-01 21:21:29 +0200

    add an example command

--- src/main/kotlin/io/github/sykq/tcc/bot/BotConfiguration.kt
+++ src/main/kotlin/io/github/sykq/tcc/bot/BotConfiguration.kt
@@ -3,10 +3,10 @@ package io.github.sykq.tcc.bot
 import io.github.sykq.tcc.ConfigurableTmiSession
 import io.github.sykq.tcc.TmiMessage
 import io.github.sykq.tcc.TmiSession
+import io.github.sykq.tcc.action.OnCommandAction
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicInteger
@@ -16,10 +16,19 @@ class BotConfiguration {
 
     @Bean
     fun messageCountingBot(): Bot {
+
         return object : Bot {
             lateinit var startTime: LocalDateTime
             val totalMessages: AtomicInteger = AtomicInteger(0)
             val subscriberMessages: AtomicInteger = AtomicInteger(0)
+            val showMessageCounterOnCommand = OnCommandAction("!messageCounter") {
+                textMessage(
+                    "counting for the last ${startTime.asUpTime().toMinutes()} minutes, ${totalMessages.get()} " +
+                            "messages have been recorded of which ${subscriberMessages.get()} were written by " +
+                            "subscribers of ${it.message.channel}. That's " +
+                            "${getMessagesPerMinute(totalMessages, startTime)} messages per minute."
+                )
+            }
 
             override val name: String = "messageCountingBot"
 
@@ -33,6 +42,7 @@ class BotConfiguration {
             }
 
             override fun onMessage(session: TmiSession, message: TmiMessage) {
+                showMessageCounterOnCommand(session, message)
                 totalMessages.getAndIncrement()
                 message.tags["subscriber"]?.also {
                     if (it.contains("1")) {
@@ -42,12 +52,10 @@ class BotConfiguration {
             }
 
             override fun getProperties(): Map<String, Any> {
-                val messagesPerMinute =
-                    totalMessages.get() / Duration.between(startTime, LocalDateTime.now()).toMinutesExact()
                 return mapOf(
                     "totalMessages" to totalMessages.get(),
                     "subscriberMessages" to subscriberMessages.get(),
-                    "messagesPerMinute" to messagesPerMinute
+                    "messagesPerMinute" to getMessagesPerMinute(totalMessages, startTime)
                 )
             }
         }
@@ -59,6 +67,14 @@ class BotConfiguration {
             lateinit var startTime: LocalDateTime
             val totalMessages: AtomicInteger = AtomicInteger(0)
             val subscriberMessages: AtomicInteger = AtomicInteger(0)
+            val showMessageCounterOnCommand = OnCommandAction("!messageCounter") {
+                textMessage(
+                    "counting for the last ${startTime.asUpTime().toMinutes()} minutes, ${totalMessages.get()} " +
+                            "messages have been recorded of which ${subscriberMessages.get()} were written by " +
+                            "subscribers of ${it.message.channel}. That's " +
+                            "${getMessagesPerMinute(totalMessages, startTime)} messages per minute."
+                )
+            }
 
             override val name: String = "reactiveMessageCountingBot"
 
@@ -67,29 +83,36 @@ class BotConfiguration {
                 // manually join a channel; alternatively the channels member can be filled with all the channels which
                 session.join("sykq")
                 // should be joined when connecting
-                session.textMessage("connected")
+//                session.textMessage("connected")
                 startTime = LocalDateTime.now()
             }
 
-            override fun onMessage(session: TmiSession, messages: Flux<TmiMessage>): Mono<Void> {
+            override fun onMessage(session: TmiSession, messages: Flux<TmiMessage>): Flux<TmiMessage> {
                 return messages.log()
-                    .doOnEach { totalMessages.getAndIncrement() }
-                    .filter { it.tags["subscriber"]?.contains("1") ?: false }
-                    .doOnEach { subscriberMessages.getAndIncrement() }
-                    .then()
+                    .doOnNext { showMessageCounterOnCommand(session, it) }
+                    .doOnNext {
+                        totalMessages.getAndIncrement()
+                        if (it.tags["subscriber"]?.contains("1") == true) {
+                            subscriberMessages.getAndIncrement()
+                        }
+                    }
             }
 
             override fun getProperties(): Map<String, Any> {
-                val messagesPerMinute =
-                    totalMessages.get() / Duration.between(startTime, LocalDateTime.now()).toMinutesExact()
                 return mapOf(
                     "totalMessages" to totalMessages.get(),
                     "subscriberMessages" to subscriberMessages.get(),
-                    "messagesPerMinute" to messagesPerMinute
+                    "messagesPerMinute" to getMessagesPerMinute(totalMessages, startTime)
                 )
             }
         }
     }
+
+    private fun LocalDateTime.asUpTime(): Duration =
+        Duration.between(this, LocalDateTime.now())
+
+    private fun getMessagesPerMinute(totalMessages: AtomicInteger, startTime: LocalDateTime): Double =
+        totalMessages.get() / startTime.asUpTime().toMinutesExact()
 
     private fun Duration.toMinutesExact(): Double = toSeconds().toDouble() / 60
 
